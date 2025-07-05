a9f2f0863d6bfa16fdaa83ebd6ebd045ef37083c
HentaiSimpLord <pierrefrancois1907@gmail.com>
2025-07-05 21:42:22 +0200

    0.1.3 new Start, Pause, Resume, Reset, Stop, GetState

--- my-timer-lib/index.js
+++ my-timer-lib/index.js
@@ -1,33 +1,63 @@
+import dayjs from "dayjs";
+import duration from "dayjs/plugin/duration";
+dayjs.extend(duration);
+
 export class Timer {
-  constructor(id) {
-    this.id = id;
+  constructor() {
     this.duration = 0;
     this.remaining = 0;
     this.intervalId = null;
     this.running = false;
+    this.paused = false;
   }
 
   start(seconds) {
+    this.stop();
     this.duration = seconds;
     this.remaining = seconds;
     this.running = true;
+    this.paused = false;
 
     this.intervalId = setInterval(() => {
-      this.remaining--;
-
-      if (this.remaining <= 0) {
-        this.stop();
+      if (!this.paused) {
+        this.remaining--;
+        if (this.remaining <= 0) {
+          this.stop();
+        }
       }
     }, 1000);
   }
 
-  timeLeft() {
-    return this.remaining;
+  pause() {
+    if (this.running && !this.paused) {
+      this.paused = true;
+    }
+  }
+
+  resume() {
+    if (this.running && this.paused) {
+      this.paused = false;
+    }
+  }
+
+  reset() {
+    this.stop();
+    this.start(this.duration);
   }
 
   stop() {
     clearInterval(this.intervalId);
     this.intervalId = null;
     this.running = false;
+    this.paused = false;
+  }
+
+  getState() {
+    return {
+      timeLeft: dayjs.duration(this.remaining, "seconds").format("mm:ss"),
+      isRunning: this.running,
+      idPaused: this.paused,
+      isFinished: !this.running && this.remaining === 0,
+    };
   }
 }
